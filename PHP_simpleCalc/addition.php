bfdf0a652fe65ed5db393bf3d427df6585afda8a
sieffosman <63376869+sieffosman@users.noreply.github.com>
2020-07-27 23:25:29 +0100

    Add files via upload

---
+++ PHP_simpleCalc/addition.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<?= "The sum of ${_GET['add_first']} and ${_GET['add_second']} is: " ?>
+
+<h4><?= $_GET["add_first"] + $_GET["add_second"] ?></h4>
+
+<a href="index.php">Reset</a>
+
+</body>
+</html>
